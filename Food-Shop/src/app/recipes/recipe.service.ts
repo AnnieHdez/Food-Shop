12157454ff9d18929a0f7641d7881279cb071345
Annie <anniehperez@gmail.com>
2020-10-23 14:34:31 -0500

    Add communication with firebase backend REST API with http requests

--- Food-Shop/src/app/recipes/recipe.service.ts
+++ Food-Shop/src/app/recipes/recipe.service.ts
@@ -5,17 +5,19 @@ import { Recipe } from './recipe.model';
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('Tasty Schnitzel',
-         'This is simply a testy recipe', 
-         'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
-         [new Ingredient ('Meat',1), new Ingredient('French Fries', 20)]),
-        new Recipe('Big Burguer', 
-        'This is simply another testy recipe',
-         'https://i.redd.it/u6axcalpx6531.jpg',
-         [new Ingredient('Bread', 2), new Ingredient('Meat', 1)])
+    // private recipes: Recipe[] = [
+    //     new Recipe('Tasty Schnitzel',
+    //      'This is simply a testy recipe', 
+    //      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg',
+    //      [new Ingredient ('Meat',1), new Ingredient('French Fries', 20)]),
+    //     new Recipe('Big Burguer', 
+    //     'This is simply another testy recipe',
+    //      'https://i.redd.it/u6axcalpx6531.jpg',
+    //      [new Ingredient('Bread', 2), new Ingredient('Meat', 1)])
 
-      ];
+    //   ];
+
+    private recipes: Recipe[] = []; 
 
     getRecipes() {
         return this.recipes.slice();
@@ -37,6 +39,11 @@ export class RecipeService{
 
     deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
         this.recipesChanged.next(this.recipes.slice());
     }
 }
